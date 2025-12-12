548951cce915171100416da290302b24211ab517
Aditya09T <adityachowdhury114@gmail.com>
2025-12-12 08:45:53 +0530

    Add SECTIONS object with HTML content

---
+++ sections.js
@@ -0,0 +1,17 @@
+const SECTIONS = {
+    proposal: `
+        <h2>💍 Proposal</h2>
+        <img src="images/proposal1.jpg">
+        <img src="images/proposal2.jpg">
+    `,
+
+    school: `
+        <h2>🏫 School Days</h2>
+        <img src="images/school1.jpg">
+    `,
+
+    memory: `
+        <h2>📸 Memories</h2>
+        <img src="images/memory1.jpg">
+    `
+};
